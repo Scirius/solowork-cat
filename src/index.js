1e8502ffe3b1b96327478c57b64aa68c48cd257c
Scirius <68830011+Scirius@users.noreply.github.com>
2020-08-13 13:01:37 +0200

    added basic html template for cat

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import "./styles.css";
 
 let htmlTemplate = `
-  <div class="cat">
-    <h1><span class="catName>Dummy</span></h1>
+    <h1><span class="catName></span></h1>
 		<input type="text" class="catStat .tiredness">
 		<input type="text" class="catStat .hunger">
 		<input type="text" class="catStat .loneliness">
-		<input type="text" class="catStat .happiness">
-	</div>`;
+    <input type="text" class="catStat .happiness">
+    `;
 
 class Cat {
   status() {
@@ -21,6 +20,17 @@ class Cat {
     this._hunger = 0;
     this._loneliness = 0;
     this._happiness = 0;
+    this.createUI();
+  }
+
+  createUI() {
+    console.log("create UI");
+    let appHolder = document.getElementById("cat-app");
+    let catHolder = document.createElement("div");
+    catHolder.className = "cat";
+    catHolder.innerHTML = htmlTemplate;
+    appHolder.appendChild(catHolder);
+    //appHolder.getElementsByClassName("catName")[0].innerHTML="Brot";
   }
 
   update() {}
