27c7cee4cce1c4a6d0e9c45a540ae89b0dc69de4
Scirius <68830011+Scirius@users.noreply.github.com>
2020-08-13 13:04:44 +0200

    fixed class-assignment to cat div corrected erroneous dots in front of classnames and class assignment to classlist.add

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "./styles.css";
 
 let htmlTemplate = `
-    <h1><span class="catName></span></h1>
-		<input type="text" class="catStat .tiredness">
-		<input type="text" class="catStat .hunger">
-		<input type="text" class="catStat .loneliness">
-    <input type="text" class="catStat .happiness">
+    <h1 class="cat-name"></h1>
+		<input type="text" class="catStat tiredness">
+		<input type="text" class="catStat hunger">
+		<input type="text" class="catStat loneliness">
+    <input type="text" class="catStat happiness">
     `;
 
 class Cat {
@@ -27,7 +27,7 @@ class Cat {
     console.log("create UI");
     let appHolder = document.getElementById("cat-app");
     let catHolder = document.createElement("div");
-    catHolder.className = "cat";
+    catHolder.classList.add("cat");
     catHolder.innerHTML = htmlTemplate;
     appHolder.appendChild(catHolder);
     //appHolder.getElementsByClassName("catName")[0].innerHTML="Brot";
